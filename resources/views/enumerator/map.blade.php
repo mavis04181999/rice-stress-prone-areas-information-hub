6391ba849308b22f700850bca14277f3aa09a1e3
Santy Sean A. Bactol <ssabactol18@gmail.com>
2022-12-06 12:34:31 +0800

    Add maps

--- resources/views/enumerator/map.blade.php
+++ resources/views/enumerator/map.blade.php
@@ -5,7 +5,7 @@
         <div class="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
 
             <div class="w-full mb-6">
-                <img class="" src="{{asset('images/Layout 7.png')}}" alt="" />
+                <img class="" src="{{asset('images/map/AlbayStressProneArea.png')}}" alt="" />
 
                 <div class="hidden sm:block" aria-hidden="true">
                     <div class="py-5">
@@ -13,7 +13,32 @@
                     </div>
                 </div>
 
-                <img class="" src="{{asset('images/Layout 8.png')}}" alt="" />
+                <img class="" src="{{asset('images/map/BicolStressProneArea.png')}}" alt="" />
+
+                <div class="hidden sm:block" aria-hidden="true">
+                    <div class="py-5">
+                        <div class="border-t border-gray-200"></div>
+                    </div>
+                </div>
+
+                <img class="" src="{{asset('images/map/CamSurStressProneArea.png')}}" alt="" />
+
+                <div class="hidden sm:block" aria-hidden="true">
+                    <div class="py-5">
+                        <div class="border-t border-gray-200"></div>
+                    </div>
+                </div>
+
+                <img class="" src="{{asset('images/map/CanamanStressProneArea.png')}}" alt="" />
+
+                <div class="hidden sm:block" aria-hidden="true">
+                    <div class="py-5">
+                        <div class="border-t border-gray-200"></div>
+                    </div>
+                </div>
+
+                <img class="" src="{{asset('images/map/TiwiStressProneArea.png')}}" alt="" />
+
             </div>
 
         </div>
